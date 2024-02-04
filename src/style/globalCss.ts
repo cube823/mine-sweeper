e7c5aec9bb03b931371fe5bec171bb47f6c84366
minkyEazel <minky@eazel.net>
2024-02-05 08:28:23 +0900

    🎨 (globalCss): Default css 초기화 코드 추가

---
+++ src/style/globalCss.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 14px;
+        vertical-align: baseline;
+    }
+    body{
+        font-family: Helvetica, Arial, sans-serif;
+    }
+    ol, ul{
+        list-style: none;
+    }
+    button {
+        border: 0;
+        background: transparent;
+        cursor: pointer;
+    }
+
+    input, textarea {
+        font-family: Helvetica, Arial, sans-serif;
+        outline: none;
+        border: none;
+        br: 0;
+    }
+`
+
+export default GlobalStyles
