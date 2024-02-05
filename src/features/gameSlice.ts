36b05548dae5e8e805ab9b9faf56cc60217fc30d
minkyEazel <minky@eazel.net>
2024-02-05 13:57:23 +0900

    🎨 (gameSlice): 클릭시 상태 업데이트 및 area-open 로직 추가

--- src/features/gameSlice.ts
+++ src/features/gameSlice.ts
@@ -1,16 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { Setting } from './levelSlice'
-import { initializeBoard } from '../utils/initializeBoard'
 
 export type GameStatus = 'ready' | 'playing' | 'won' | 'lost'
 
 export interface ICell {
   type: 'veiled' | 'flagged' | 'question' | 'mine' | 'unveiled'
   isMine: boolean
+  neighborMines?: number
 }
 
 interface GameSlice {
+  cells: Coord[]
+
   board: ICell[][]
   gameStatus: GameStatus
 }
@@ -21,8 +23,60 @@ export type Coord = {
 }
 
 const initialState: GameSlice = {
-  board: initializeBoard(20, 12, 10),
+  cells: [],
+  board: [],
   gameStatus: 'ready',
+}
+
+const getCount = (board: ICell[][], coord: Coord) => {
+  const { x, y } = coord
+  let count = 0
+  for (let yOffset = -1; yOffset <= 1; yOffset++) {
+    for (let xOffset = -1; xOffset <= 1; xOffset++) {
+      const newY = y + yOffset
+      const newX = x + xOffset
+      if (newY >= 0 && newY < board.length && newX >= 0 && newX < board[0].length) {
+        if (board[newY][newX].isMine) {
+          count++
+        }
+      }
+    }
+  }
+
+  return count
+}
+
+const areaOpen = (startPosition: Coord, board: ICell[][]) => {
+  const stack: Coord[] = [startPosition]
+
+  while (stack.length) {
+    const coord = stack.pop()
+    if (!coord) return board
+
+    const { x, y } = coord
+
+    if (y < 0 || y >= board.length) continue
+    if (x < 0 || x >= board[0].length) continue
+
+    const cell = board[y][x]
+    if (cell.type === 'unveiled') continue
+
+    const count = getCount(board, coord)
+
+    board[y][x].type = 'unveiled'
+    board[y][x].neighborMines = count
+
+    if (!cell.isMine && count === 0) {
+      for (let dy = -1; dy <= 1; dy++) {
+        for (let dx = -1; dx <= 1; dx++) {
+          if (dx === 0 && dy === 0) continue
+          stack.push({ x: x + dx, y: y + dy })
+        }
+      }
+    }
+  }
+
+  return board
 }
 
 const gameSlice = createSlice({
@@ -31,6 +85,7 @@ const gameSlice = createSlice({
   reducers: {
     populateBoard: (state, action: PayloadAction<Setting>) => {
       state.board = []
+      state.gameStatus = 'ready'
 
       const cells: Coord[] = []
       for (let y = 0; y < action.payload.columns; y++) {
@@ -41,12 +96,27 @@ const gameSlice = createSlice({
         }
       }
 
-      for (let mineCount = 0; mineCount < action.payload.mines; mineCount++) {
+      state.cells = cells
+    },
+
+    startGame: (
+      state,
+      { payload: { startPosition, mines } }: PayloadAction<{ startPosition: Coord; mines: number }>
+    ) => {
+      state.gameStatus = 'playing'
+      const cells = [...state.cells].filter(
+        (cell) => cell.x !== startPosition.x || cell.y !== startPosition.y
+      )
+
+      for (let mineCount = 0; mineCount < mines; mineCount++) {
         const index = Math.floor(Math.random() * cells.length)
         const { x, y } = cells[index]
         state.board[y][x].isMine = true
         cells.splice(index, 1)
       }
+
+      const newBoard = areaOpen(startPosition, state.board)
+      state.board = newBoard
     },
 
     makeFlag: (state, action: PayloadAction<Coord>) => {
@@ -60,21 +130,33 @@ const gameSlice = createSlice({
           break
         case 'question':
           state.board[y][x].type = 'veiled'
-          break
-        default:
-          return
       }
     },
 
     revealCell: (state, action: PayloadAction<Coord>) => {
       const { x, y } = action.payload
-      if (state.board[y][x].type === 'veiled') {
+      if (state.gameStatus !== 'playing') return
+      if (state.board[y][x].type !== 'veiled') return
+
+      if (state.board[y][x].isMine) {
         state.board[y][x].type = 'unveiled'
+        state.gameStatus = 'lost'
+        return
       }
+
+      const count = getCount(state.board, action.payload)
+
+      if (count === 0) {
+        state.board = areaOpen(action.payload, state.board)
+        return
+      }
+
+      state.board[y][x].neighborMines = count
+      state.board[y][x].type = 'unveiled'
     },
   },
 })
 
-export const { populateBoard, makeFlag, revealCell } = gameSlice.actions
+export const { populateBoard, makeFlag, startGame, revealCell } = gameSlice.actions
 
 export default gameSlice.reducer
