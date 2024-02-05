705ee07ccadc0bf47bcd3ec2938b6c5f8738717b
minkyEazel <minky@eazel.net>
2024-02-05 11:49:19 +0900

    🎨 (gameSlice): 지뢰찾기 게임판 global State 추가

--- src/features/gameSlice.ts
+++ src/features/gameSlice.ts
@@ -1,23 +1,80 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
+import { Setting } from './levelSlice'
+import { initializeBoard } from '../utils/initializeBoard'
 
-interface GameSlice {}
+export type GameStatus = 'ready' | 'playing' | 'won' | 'lost'
 
-const initialState: GameSlice = {}
+export interface ICell {
+  type: 'veiled' | 'flagged' | 'question' | 'mine' | 'unveiled'
+  isMine: boolean
+}
+
+interface GameSlice {
+  board: ICell[][]
+  gameStatus: GameStatus
+}
+
+export type Coord = {
+  x: number
+  y: number
+}
+
+const initialState: GameSlice = {
+  board: initializeBoard(20, 12, 10),
+  gameStatus: 'ready',
+}
 
 const gameSlice = createSlice({
   name: 'gameSlice',
   initialState,
   reducers: {
-    // updateHederTitle: (state, action: PayloadAction<string>) => {
-    //   state.title.text = action.payload
-    // },
-    // updateHederDescription: (state, action: PayloadAction<string>) => {
-    //   state.description.text = action.payload
-    // },
+    populateBoard: (state, action: PayloadAction<Setting>) => {
+      state.board = []
+
+      const cells: Coord[] = []
+      for (let y = 0; y < action.payload.columns; y++) {
+        state.board.push([])
+        for (let x = 0; x < action.payload.rows; x++) {
+          cells.push({ x, y })
+          state.board[y].push({ type: 'veiled', isMine: false })
+        }
+      }
+
+      for (let mineCount = 0; mineCount < action.payload.mines; mineCount++) {
+        const index = Math.floor(Math.random() * cells.length)
+        const { x, y } = cells[index]
+        state.board[y][x].isMine = true
+        cells.splice(index, 1)
+      }
+    },
+
+    makeFlag: (state, action: PayloadAction<Coord>) => {
+      const { x, y } = action.payload
+      switch (state.board[y][x].type) {
+        case 'veiled':
+          state.board[y][x].type = 'flagged'
+          break
+        case 'flagged':
+          state.board[y][x].type = 'question'
+          break
+        case 'question':
+          state.board[y][x].type = 'veiled'
+          break
+        default:
+          return
+      }
+    },
+
+    revealCell: (state, action: PayloadAction<Coord>) => {
+      const { x, y } = action.payload
+      if (state.board[y][x].type === 'veiled') {
+        state.board[y][x].type = 'unveiled'
+      }
+    },
   },
 })
 
-export const {} = gameSlice.actions
+export const { populateBoard, makeFlag, revealCell } = gameSlice.actions
 
 export default gameSlice.reducer
