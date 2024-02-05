11388d7dd285dd3fe3721b3c1dde5eb025b7d123
minkyEazel <minky@eazel.net>
2024-02-05 10:06:59 +0900

    🎨 (display-board): 지뢰수, 타임클락 Display 컴포넌트 추가

---
+++ src/components/Header/DisplayBoard/index.tsx
@@ -0,0 +1,37 @@
+import { styled } from 'styled-components'
+
+type DisplayBoardProps = {
+  value: number
+}
+
+const imgSrcset = [
+  '/time_zero.png',
+  '/time_one.png',
+  '/time_two.png',
+  '/time_three.png',
+  '/time_four.png',
+  '/time_five.png',
+  '/time_six.png',
+  '/time_seven.png',
+  '/time_eight.png',
+  '/time_nine.png',
+]
+
+function DisplayBoard({ value }: DisplayBoardProps) {
+  return (
+    <div>
+      <Image src={imgSrcset[Math.floor(value / 100) % 10]} />
+      <Image src={imgSrcset[Math.floor(value / 10) % 10]} />
+      <Image src={imgSrcset[value % 10]} />
+    </div>
+  )
+}
+
+const Image = styled.img`
+  width: 13px;
+  height: 23px;
+  display: inline-block;
+  image-rendering: pixelated;
+`
+
+export default DisplayBoard
