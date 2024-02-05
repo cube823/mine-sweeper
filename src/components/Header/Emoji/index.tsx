0842261739d165f0262c2a1c20db76829c56a5fb
minkyEazel <minky@eazel.net>
2024-02-05 13:54:40 +0900

    🎨 (emoji): Lost / 그 외 상태 변경 및 재실행 로직 추가

--- src/components/Header/Emoji/index.tsx
+++ src/components/Header/Emoji/index.tsx
@@ -1,14 +1,29 @@
+import { useMemo } from 'react'
 import { styled } from 'styled-components'
+import { populateBoard } from '../../../features/gameSlice'
+import { useAppDispatch, useAppSelector } from '../../../store'
 
 const Emoji = () => {
-  return <Image />
+  const dispatch = useAppDispatch()
+  const { gameStatus } = useAppSelector((state) => state.gameReducer)
+  const { setting } = useAppSelector((state) => state.levelReducer)
+
+  const src = useMemo(() => {
+    if (gameStatus === 'lost') return '/facedead.gif'
+    return '/facesmile.gif'
+  }, [gameStatus])
+
+  const onReset = () => dispatch(populateBoard(setting))
+
+  return <Image onClick={onReset} src={src} alt='reset-emoji-button' />
 }
 
 const Image = styled.img`
-  width: 13px;
-  height: 23px;
+  width: 26px;
+  height: 26px;
   display: inline-block;
   image-rendering: pixelated;
+  cursor: pointer;
 `
 
 export default Emoji
